55f46c8cc15e7c9e5ef8bb41da5b0a56c2ca2d9a
Alexandre CR <devalexandrecr@gmail.com>
2025-11-24 15:49:26 +0000

    remove: libi-front Vue.js application

--- src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="text-center">
-      <h1 class="text-4xl font-bold mb-4 text-primary">{{ $t('home.title') }}</h1>
-      <p class="mb-8">{{ $t('home.subtitle') }}</p>
-      <router-link to="/login" class="bg-primary text-white px-6 py-2 rounded">{{ $t('home.cta') }}</router-link>
-    </div>
-  </div>
-</template>
